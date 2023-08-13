f63e792372017d14464f0af946f524ab436da69f
Samuel <samueldflima@protonmail.com>
2023-08-13 18:13:17 -0300

    just push 2

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,9 @@
-import { Poppins } from 'next/font/google'
 import NextNProgress from 'nextjs-progressbar'
 import '../styles/globals.css'
 
-const poppins = Poppins({ subsets: ['latin'], weight: '400' })
-
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main className="{poppins.className}">
+    <main>
       <NextNProgress color="#1DB954" stopDelayMs={0} />
       <Component {...pageProps} />
     </main>
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,50 +1,62 @@
 import Link from "next/link";
-import GoogleIcon from '@mui/icons-material/Google';
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import AppleIcon from '@mui/icons-material/Apple';
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import AppleIcon from "@mui/icons-material/Apple";
 
 export default function LoginPage() {
   return (
-    <main className="bg-gradient-to-b from-zinc-900">
-      <form className="flex flex-col bg-black w-[700px] h-[850px] mt-7 mx-auto py-14 px-20 rounded-md items-center">
-        <h1 className="text-white font-bold text-[40px]">
-          Entrar no Spotify
-        </h1>
+    <main className="h-screen bg-gradient-to-b from-zinc-900">
+      <form className="flex flex-col bg-black w-[700px] h-full mx-auto py-14 px-20 rounded-md items-center">
+        <h1 className="text-white font-bold text-[40px]">Entrar no Spotify</h1>
 
-        <div className="flex flex-col space-y-2 m-10 px-16">
-          <button
-            type="button"
-            className="text-white font-semibold border-2 border-zinc-400 rounded-3xl p-2.5 hover:border-white"
-          >
-          <GoogleIcon className="mr-2"/>
-            Continuar com o google
-          </button>
-          <button
-            type="button"
-            className="text-white font-semibold border-2 border-zinc-400 rounded-3xl p-2.5 hover:border-white"
-          >
-            <FacebookOutlinedIcon className="mr-2"/>
-            Continuar com o facebook
-          </button>
-          <button
-            type="button"
-            className="text-white font-semibold border-2 border-zinc-400 rounded-3xl p-2.5 hover:border-white"
-          >
-            <AppleIcon className="mr-2"/>
-            Continuar com a Apple
-          </button>
-          <button
-            type="button"
-            className="text-white font-semibold border-2 border-zinc-400 rounded-full p-3 hover:border-white"
-          >
-            Continuar com um número de telefone
-          </button>
-        </div>
+        <ul className="flex flex-col space-y-2 mt-10 mb-2 px-32">
+          <li>
+            <button
+              type="button"
+              className="text-white font-semibold border-2 border-zinc-400 rounded-3xl w-full hover:border-white flex px-4 py-2"
+            >
+              <GoogleIcon className="" />
+              <span className="mx-auto">Continuar com o google</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="text-white font-semibold border-2 border-zinc-400 rounded-3xl w-full hover:border-white flex px-4 py-2"
+            >
+              <FacebookOutlinedIcon className="" />
+              <span className="mx-auto">Continuar com o facebook</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="text-white font-semibold border-2 border-zinc-400 rounded-3xl w-full hover:border-white flex px-4 py-2"
+            >
+              <AppleIcon className="" />
+              <span className="mx-auto">Continuar com a apple</span>
+            </button>
+          </li>
+
+          <li>
+            <button
+              type="button"
+              className="text-white font-semibold border-2 border-zinc-400 rounded-full w-full hover:border-white"
+            >
+              Continuar com um número de telefone
+            </button>
+          </li>
+        </ul>
 
         <hr className="border-zinc-900 w-full m-10" />
 
         <div className="w-full flex flex-col px-28">
-          <label htmlFor="email-input" className="mb-2 text-white font-semibold">
+          <label
+            htmlFor="email-input"
+            className="mb-2 text-white font-semibold"
+          >
             E-mail ou nome de usuário
           </label>
           <input
@@ -54,7 +66,10 @@ export default function LoginPage() {
             className="w-full py-2 px-5 text-white text-sm rounded-sm bg-[#131313] border-2 border-zinc-400 hover:border-white focus:border-white focus:outline-none"
           />
 
-          <label htmlFor="password-input" className="mt-4 mb-2 text-white font-semibold">
+          <label
+            htmlFor="password-input"
+            className="mt-4 mb-2 text-white font-semibold"
+          >
             Senha
           </label>
           <input
@@ -64,9 +79,9 @@ export default function LoginPage() {
             className="w-full py-2 px-5 text-white text-sm rounded-sm bg-[#131313] border-2 border-zinc-400 hover:border-white focus:border-white focus:outline-none"
           />
 
-          <label className="relative inline-flex items-center">
+          <label className="mt-8 relative inline-flex items-center">
             <input type="checkbox" className="sr-only peer" />
-            <div className="w-8 h-4 bg-zinc-400 rounded-full peer peer-focus:ring-2 peer-focus:ring-white peer-checked:after:translate-x-4 peer-checked:after:border-none after:absolute after:top-1 after:left-[2px] after:bg-black after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-[#1db954]"></div>
+            <div className="w-8 h-4 bg-zinc-400 rounded-full peer peer-focus:ring-4 peer-focus:ring-white peer-checked:after:translate-x-4 peer-checked:after:border-none after:absolute after:top-1 after:left-[2px] after:bg-black after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-[#1db954]"></div>
             <span className="ml-3 text-sm font-medium text-white">
               Lembrar de mim
             </span>
@@ -74,14 +89,14 @@ export default function LoginPage() {
 
           <button
             type="button"
-            className="h-10 w-full mt-10 text-white font-semibold bg-[#1db954] rounded-3xl outline-white hover:border-2 hover:border-white"
+            className="p-3 mt-10 mb-5 text-black hover:scale-[1.04] font-semibold bg-[#1db954] rounded-3xl outline-white "
           >
             Entrar
           </button>
 
           <Link
-            href="#!"
-            className="text-white text-center underline underline-offset-1 hover:text-[#1db954]"
+            href="https://accounts.spotify.com/en/password-reset?flow_ctx=578b03af-64b8-448c-949e-dfd77fc8fbf5%3A1691979769"
+            className="mb-8 text-white text-center underline underline-offset-1 hover:text-[#1db954]"
           >
             Esqueceu sua senha?
           </Link>
@@ -89,7 +104,7 @@ export default function LoginPage() {
 
         <hr className="border-zinc-900 w-full" />
 
-        <p className="text-zinc-400 pb-10">
+        <p className="mt-10 text-zinc-400 pb-10">
           Não tem uma conta?
           <Link
             href="#!"
